54ca0c397ac2d19aec82dde603fab1e61b20923d
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-01-07 08:33:52 +0200

    Installed Chakra UI and dependecies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react"
+
+function App() {
+  // eslint-disable-next-line react-hooks/immutability
+  document.title = "Hamdiola"
+  return(
+    <Button variant="subtle">Subtle</Button>
+  )
+}
+
+export default App
