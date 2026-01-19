afb14448b6e368f12ae04ee2dc3782c4ebb30bb0
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-01-19 20:09:36 +0200

    Made responsive layout using Grid and GridItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
-  // eslint-disable-next-line react-hooks/immutability
-  document.title = "Hamdiola"
   return(
-    <Button variant="subtle">Subtle</Button>
+  <Grid templateAreas={
+    {base: `"nav" "main"`,
+     lg: `"nav nav" "aside main"`}
+  }>
+      <GridItem area = "nav" bg={`red`}>NAV</GridItem>
+      <GridItem 
+      area = "aside" bg={`gold`}
+      display={{ base: "none", lg: "block" }}
+      >
+        ASIDE</GridItem>
+      <GridItem area = "main" bg={`coral`}>MAIN</GridItem>
+  </Grid>
   )
 }
 
